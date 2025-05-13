446bb47b1d075caaf2e4c0422a6b3914d9b8de8a
Nparrita10 <nicoparra10@hotmail.com>
2025-05-13 11:31:21 -0300

    Delete LiebreBet directory

--- LiebreBet/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ApuestasSimuladas from './src/ApuestasSimuladas.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ApuestasSimuladas />
-  </React.StrictMode>,
-);
